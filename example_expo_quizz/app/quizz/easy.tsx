d59c93e843c3785d4925bacaef9dc0123c8455c7
Julien POIRIER <poirierjulien@live.fr>
2026-02-11 12:21:28 +0100

    tp

--- example_expo_quizz/app/quizz/easy.tsx
+++ example_expo_quizz/app/quizz/easy.tsx
@@ -1,5 +1,21 @@
 import { Text } from "react-native";
 
+/*
+
+Utiliser des hooks pour récupérer les questions a l'url : https://opentdb.com/api.php?amount=10&type=boolean
+Afficher ces questions avec les composants react native :
+https://reactnative.dev/docs/view VIEW pour avoir toute la question dedans.
+https://reactnative.dev/docs/button BUTTON pour afficher les réponses cliquables.
+https://reactnative.dev/docs/text TEXT pour afficher la question en elle-même.
+
+Il faut gérer le score pour savoir si l'utilisateur trouve la bonne réponse ou non.
+
+
+Si vous arrivez a faire apparaitre une question par une question,
+c'est mieux. (cela suppose de garder en mémoire
+dans le composant la question en cours, toutes les questions, le score, etc.)
+
+*/
 const Easy = () => {
   return <Text>Trop simple</Text>;
 };
